a2d4061c0726f04fb4031b463ee21fc4901f1d89
Leo Vidal <vidaldl@outlook.com>
2023-12-08 12:37:28 -0700

    Set up the settings so they store locally

--- scripts/associateSectionsToBlueprints/associatedBpCourse.js
+++ scripts/associateSectionsToBlueprints/associatedBpCourse.js
@@ -1,60 +1,39 @@
-// Link Associated Blueprint Courses
-function associatedBpCourse() {
-    console.log("runninggg");
-    function blueprintAssociations() {
-        // To target the span that contains the associated courses which is in tr
-        let associatedCourses = document.querySelectorAll(
-            'span[dir="ltr"] .bca-associations-table tr[id^="course_"]'
-        );
+function blueprintAssociations() {
+    let associatedCourses = document.querySelectorAll('span[dir="ltr"] .bca-associations-table tr[id^="course_"]');
 
-        associatedCourses.forEach((v) => {
-            let courseId = v.id.split('_')[1];
-            let linkSpan = v.querySelector('td span');
-            let html = linkSpan.innerHTML;
-            if (
-                !html.includes(
-                    `<a href="/courses/${courseId}" target="_blank">${html}</a>`
-                )
-            ) {
-                // Make the textContent in the span a link
-                linkSpan.innerHTML = `<a href="/courses/${courseId}" target="_blank">${html}</a>`;
-            }
-        });
-    }
-
-    // If the Associations in the bp button is clicked which
-    // make the blueprintAssociations truthy, execute the waitFor
-    try {
-        setTimeout(() => {
-            if (blueprintAssociations) {
-                var observer = new MutationObserver(() => {
-                    // Check if the user has navigated to Associations
-                    if (
-                        document.querySelectorAll(
-                            'span[dir="ltr"] .bca-associations-table tr[id^="course_"] span'
-                        ) &&
-                        document.querySelectorAll(
-                            'span[dir="ltr"] .bca-associations-table tr[id^="course_"] span'
-                        ).length > 0
-                    ) {
-                        observer.disconnect();
-                        blueprintAssociations();
-                    }
-                    observer.observe(document, {
-                        childList: true,
-                        subtree: true,
-                    });
-                });
-                observer.observe(document, {
-                    childList: true,
-                    subtree: true,
-                });
-                return observer;
-            }
-        }, 1000);
-    } catch (error) {
-        console.warn(error);
-    }
+    associatedCourses.forEach(v => {
+        let courseID = v.id.split('_')[1];
+        let linkSpan = v.querySelector('td span');
+        let html = linkSpan.innerHTML;
+        if (!html.includes(`<a href="/courses/${courseID}" target="_blank">${html}</a>`)) {
+            linkSpan.innerHTML = `<a href="/courses/${courseID}" target="_blank">${html}</a>`;
+        }
+    });
 }
-associatedBpCourse();
 
+function waitFor(parent, fn, cb) {
+    var observer = new MutationObserver(() => {
+        if (fn()) {
+            observer.disconnect();
+            cb();
+        }
+        observer.observe(parent, {
+            childList: true,
+            subtree: true,
+        });
+    });
+    observer.observe(parent, {
+        childList: true,
+        subtree: true,
+    });
+    return observer;
+}
+
+chrome.storage.sync.get({
+    blueprintAssociations: false,
+}, function (items) {
+    if (items.blueprintAssociations === true) {
+        waitFor(document, () => document.querySelectorAll('span[dir="ltr"] .bca-associations-table tr[id^="course_"] span').length > 0, blueprintAssociations);
+
+    }
+});
