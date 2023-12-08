1484352b198a0acd9b65c8326f39c22c1078fa5c
noahcarrier1999 <car20111@byui.edu>
2023-12-08 11:59:07 -0700

    I started making the code cleaner

--- scripts/copyAllBrokenLinks/contentScript.js
+++ scripts/copyAllBrokenLinks/contentScript.js
@@ -1,43 +1,76 @@
 
+
+/****
+ *
+ * This function is to scrape the affected URL for the following.
+ *      - The Title of all Broken Links on Page 
+ *      - The URL's of all Broken Links on the Page
+ *      - The Page Location of all Broken Links on the Page
+ *
+ * Proccess:
+ * I. Scrapes the Data From the Affected URL
+ * II. Stores Data into Arrays and sends the Arrays as a message to the local background.js script.
+ *
+ * Affected URLs:
+ * [canvas_instance]/course/[courseId]/link_validator
+ *
+ ****/
+function scrape() {
+
+    let arrayOfPageUrls = selectHeaderHREFs();
+
+    let arrayOfBrokenLinks = selectLinksHREFs();
+    
+    let arrayOfLinkTitles = selectAllTitles();
+
+    if (arrayOfPageUrls && arrayOfBrokenLinks && arrayOfLinkTitles){
+        chrome.runtime.sendMessage({
+          action: "sendArraysToBackground", 
+          array1:arrayOfPageUrls, 
+          array2:arrayOfBrokenLinks, 
+          array3:arrayOfLinkTitles});
+    
+    } else {
+        console.log("Data retrival failed.")
+    }
+}
+
+
+
+
+/***
+ * 
+ *  Utility Functions
+ * 
+ *  selectHeaderHREF
+***/
+
 //selects all the page urls on the page and stores it in an array
 function selectHeaderHREFs(){
 
   let PageUrlArray = [];
-  let divs = document.querySelectorAll('div.result');
-  
 
-  //console.log(divs);
+  let divs = document.querySelectorAll('div.result');
 
   let linksAmmount = Array.from(divs).map(div => {
       let links = div.querySelectorAll('a'); 
       let count = links.length - 1;
-      //console.log(count)
 
       if (count > 1){
-
           let headings = document.querySelector('.result h2 > a');
           let link = headings.getAttribute('href');
 
           for (let i = 0; i <= count - 1; i++){
-              
-              //console.log(link);
               PageUrlArray.push(link);
-              
           }      
       }
       else 
       {
-          //console.log(links[0].getAttribute("href"));
           PageUrlArray.push(links[0].getAttribute("href"));
       }
-
-      //console.log(PageUrlArray);
-      
   });
 
   return PageUrlArray;
-
-  
 };
 
 //selects all of the brokenlinks on the page and stores it in an array
@@ -55,46 +88,10 @@ function selectLinksHREFs() {
 function selectAllTitles()
 {
   const titles = document.querySelectorAll('.result ul > li > a');
-  //console.log(titles);
+  
   let titlesArray = Array.from(titles).map(title => title.textContent);
 
   return titlesArray;
-}
-
-//uses each of the function to collect all of the arrays and sends a message containing
-//all of them to the background script
-function scrape() {
-    // Execute the content script in the active tab
-    console.log("Received 'startScraping' message");
-
-    // This code runs in the context of the active tab
-
-    let arrayOfPageUrls = selectHeaderHREFs();
-    console.log('Pages:');
-    console.log(arrayOfPageUrls);
-
-    let arrayOfBrokenLinks = selectLinksHREFs();
-    console.log('broken:');
-    console.log(arrayOfBrokenLinks);
-    
-    let arrayOfLinkTitles = selectAllTitles();
-    console.log('link text');
-    console.log(arrayOfLinkTitles);
-
-    if (arrayOfPageUrls && arrayOfBrokenLinks && arrayOfLinkTitles){
-        console.log("Data retrieved Successfully");
-        chrome.runtime.sendMessage({
-          action: "sendArraysToBackground", 
-          array1:arrayOfPageUrls, 
-          array2:arrayOfBrokenLinks, 
-          array3:arrayOfLinkTitles});
-    
-    } else {
-        console.log("Data retrival failed.")
-    }
-
-
-    
 }
 
 
