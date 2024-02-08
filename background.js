44b0612531a62df08cf44646a3517d1a1aded61a
noahcarrier1999 <car20111@byui.edu>
2024-02-08 15:13:17 -0700

    added more functionalities

--- background.js
+++ background.js
@@ -14,3 +14,65 @@ import { copyAllBrokenLinks } from './scripts/copyAllBrokenLinks/background.js';
  ****/
  copyAllBrokenLinks();
 
+/********************************
+ *
+ *
+ *
+ ********************************/
+let thing = '';
+chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason === 'install') {
+        console.log("First Load");
+
+        chrome.runtime.openOptionsPage();
+        thing = 'installed';
+    } else if (details.reason == 'update') {
+        console.log('Update');
+        chrome.runtime.openOptionsPage();
+        thing = 'updated';
+    } else {
+        console.log(details);
+
+    }
+    addEmailOption();
+});
+
+/********************************
+ *
+ *
+ *
+ ********************************/
+function firstLoad() {
+    let returnValue = thing;
+    thing = '';
+    return returnValue;
+}
+
+/********************************
+ *
+ * Adds right-click menu option to
+ * copy student emails to clipboard.
+ *
+ ********************************/
+function addEmailOption() {
+    chrome.contextMenus.create({
+        id: "email-students",
+        title: "Email Students On This List",
+        contexts: ["page"],
+        documentUrlPatterns: ["https://*.instructure.com/courses/*/quizzes/*/statistics"]
+    });
+
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+        if (info.menuItemId == "email-students") {
+            chrome.tabs.query({
+                "active": true,
+                "currentWindow": true
+            }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    "functiontoInvoke": "emailStudents"
+                });
+            });
+        }
+    });
+}
+
