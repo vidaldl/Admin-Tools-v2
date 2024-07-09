66b0579d837a6b34ffe66103ffffbc4806ddb7a7
noahcarrier1999 <car20111@byui.edu>
2024-07-09 13:07:12 -0600

    Added a way to fix divs in questionBanks

---
+++ scripts/addDivsToQuizQuestions/background.js
@@ -0,0 +1,34 @@
+export function addDivsToQuestions() {
+  // This event is fired with the user accepts the permission.
+  chrome.runtime.onInstalled.addListener(function () {
+    // Create a context menu
+    chrome.contextMenus.create({
+      id: "unfiled-questions",
+      title: "Add Div to Unfiled Questions",
+      contexts: ["page"],
+      documentUrlPatterns: [
+        "https://*.instructure.com/courses/*/question_banks/*",
+      ],
+    });
+  });
+
+  // Listener for context menu click
+  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "unfiled-questions") {
+      chrome.tabs.sendMessage(
+        tab.id,
+        { type: "ADD_DIV", content: "Trigger Content Script" },
+        function (response) {
+          if (chrome.runtime.lastError) {
+            console.error(
+              "Error sending message to content script:",
+              chrome.runtime.lastError.message
+            );
+          } else {
+            console.log("Response form the content Script:", response);
+          }
+        }
+      );
+    }
+  });
+}
